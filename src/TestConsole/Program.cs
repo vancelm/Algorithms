0c16062d9b98cde71abdf65976cfece03483b385
Vance Morgan <vance@vancemorgan.com>
2021-06-16 13:14:38 -0500

    Added simple test framework to allow for different types of algorithm testing

--- src/TestConsole/Program.cs
+++ src/TestConsole/Program.cs
@@ -2,11 +2,35 @@
 
 namespace TestConsole
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args[0] != null)
+            {
+                switch (args[0].Trim().ToLowerInvariant())
+                {
+                    case "sorting":
+                        RunSortingTests();
+                        break;
+                    case "binomial-coefficient":
+                        RunBinomialCoefficientTests();
+                        break;
+                    default:
+                        Console.WriteLine("Unknown test");
+                        break;
+                }
+            }
+        }
+
+        private static void RunSortingTests()
+        {
+
+        }
+
+        private static void RunBinomialCoefficientTests()
+        {
+
         }
     }
 }
