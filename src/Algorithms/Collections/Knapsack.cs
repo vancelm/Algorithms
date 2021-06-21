e3d6c1ee4d0cd58afa7cd09f15231070c5f9038d
Vance Morgan <vance@vancemorgan.com>
2021-06-21 13:21:52 -0500

    Implenent IList<T>

--- src/Algorithms/Collections/Knapsack.cs
+++ src/Algorithms/Collections/Knapsack.cs
@@ -1,6 +1,64 @@
-﻿namespace Algorithms.Collections
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithms.Collections
 {
-    public class Knapsack
+    public class Knapsack : IList<(int Value, int Weight)>
     {
+        public (int Value, int Weight) this[int index] { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public int Count => throw new System.NotImplementedException();
+
+        public bool IsReadOnly => throw new System.NotImplementedException();
+
+        public void Add((int Value, int Weight) item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains((int Value, int Weight) item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo((int Value, int Weight)[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<(int Value, int Weight)> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int IndexOf((int Value, int Weight) item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(int index, (int Value, int Weight) item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove((int Value, int Weight) item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
