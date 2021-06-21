41e5db0a5338bb778c16486e6a9d377d75c3325e
Vance Morgan <vance@vancemorgan.com>
2021-06-21 13:24:39 -0500

    Added constructor and paramters for storing weight and capacity.

--- src/Algorithms/Collections/Knapsack.cs
+++ src/Algorithms/Collections/Knapsack.cs
@@ -7,6 +7,14 @@ namespace Algorithms.Collections
     {
         private List<(int Value, int Weight)> _list = new List<(int, int)>();
 
+        public int Capacity { get; }
+        public int Weight { get; private set; }
+
+        public Knapsack(int capacity)
+        {
+            Capacity = capacity;
+        }
+
         public (int Value, int Weight) this[int index] { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
         public int Count => throw new System.NotImplementedException();
