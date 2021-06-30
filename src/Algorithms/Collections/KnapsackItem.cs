f9b25291ce8de4b975da20f27781830bf3a38682
Vance Morgan <vance@vancemorgan.com>
2021-06-30 02:04:23 -0500

    Added KnapsackItem.cs

---
+++ src/Algorithms/Collections/KnapsackItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Algorithms
+{
+    public struct KnapsackItem
+    {
+        public readonly int Value;
+        public readonly int Weight;
+        public readonly int Profit;
+
+        public KnapsackItem(int value, int weight)
+        {
+            Value = value;
+            Weight = weight;
+            Profit = value / weight;
+        }
+    }
+}
