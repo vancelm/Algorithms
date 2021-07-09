7f260bf5a64019741008de3b49e7fa7aaf3b209c
Vance Morgan <vance@vancemorgan.com>
2021-07-09 17:22:01 -0500

    Refactored to use static test classes

--- src/TestConsole/Program.cs
+++ src/TestConsole/Program.cs
@@ -12,28 +12,24 @@ namespace TestConsole
                 return;
             }
 
-            Action test;
-            Func<bool> validate;
-
             switch (args[0].Trim().ToLowerInvariant())
             {
                 case "sort":
-                    validate = () => SortTest.Validate();
-                    test = () => SortTest.Test();
+                    SortTest.Validate();
+                    SortTest.Test();
                     break;
                 case "binomial-coefficient":
-                    test = new BinomialCoefficientTest();
+                    BinomialCoefficientTest.Validate();
+                    BinomialCoefficientTest.Test();
                     break;
-                case "knapsack":
-                    test = new KnapsackTest();
+                case "knapsack1":
+                    KnapsackTest.Validate();
+                    KnapsackTest.Test1();
                     break;
                 default:
                     Console.WriteLine("Unknown test specified.");
                     return;
             }
-
-            AlgorithmTestHelper.Validate(validate);
-            AlgorithmTestHelper.Test(test);
         }
     }
 }
