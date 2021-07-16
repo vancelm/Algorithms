8149440905d095fa5224657d89eccc81829bca8b
Vance Morgan <vance@vancemorgan.com>
2021-07-16 06:34:58 -0500

    Added tests for SearchTest

--- src/TestConsole/SearchTest.cs
+++ src/TestConsole/SearchTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using static Algorithms.Search;
+using static TestConsole.AlgorithmTestHelper;
 
 namespace TestConsole
 {
@@ -12,7 +15,9 @@ namespace TestConsole
 
         private static void Validate()
         {
+            Console.WriteLine("Validating...");
             ValidateSearch(SequentialSearch, "Sequential");
+            ValidateSearch(BinarySearch, "Binary");
         }
 
         private static void ValidateSearch(Func<int, IList<int>, int> search, string name)
@@ -21,14 +26,75 @@ namespace TestConsole
 
             Console.Write($"{name}: ");
             if (index == validationIndex)
-                Console.Write("<PASS>");
+                Console.WriteLine("<PASS>");
             else
-                Console.Write("<FAIL>");
+                Console.WriteLine("<FAIL>");
         }
 
         public static void Test()
         {
             Validate();
+            Test1();
+            Test2();
+        }
+
+        private static void Test1()
+        {
+            List<Func<int, List<int>, int>> searches = new();
+            searches.Add(SequentialSearch);
+            searches.Add(BinarySearch);
+
+            Console.WriteLine("Test 1 (evenly distributed search values)");
+            Console.WriteLine(" Count, Sequential,     Binary");
+
+            for (int i = 10000; i <= 100000; i += 10000)
+            {
+                Console.Write($"{i,6}");
+
+                List<int> list = GetSequentialList(i);
+                foreach (var search in searches)
+                {
+                    double elapsed = TimeAlgorithm(() =>
+                    {
+                        for (int j = 0; j < i; j += i / 100)
+                        {
+                            search(j, list);
+                        }
+                    });
+
+                    Console.Write($", {elapsed,10:0.0000}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void Test2()
+        {
+            List<Func<int, List<int>, int>> searches = new();
+            searches.Add(SequentialSearch);
+            searches.Add(BinarySearch);
+
+            Console.WriteLine("Test 2 (worst case)");
+            Console.WriteLine(" Count, Sequential,     Binary");
+
+            for (int i = 10000000; i <= 100000000; i += 10000000)
+            {
+                Console.Write($"{i,6}");
+
+                List<int> list = GetSequentialList(i);
+                foreach (var search in searches)
+                {
+                    double elapsed = TimeAlgorithm(() =>
+                    {
+                        search(i, list);
+                    });
+
+                    Console.Write($", {elapsed,10:0.0000}");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
